14b727ab6a20ab92f0c8c47744537c30626d1017
Vasyokkrut <vasiliybytenko@gmail.com>
2021-01-09 09:12:49 +0300

    indentations changed from 4 spaces to 2 spaces

--- src/Components/FullPicture.jsx
+++ src/Components/FullPicture.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 function FullPicture(props) {
-    document.getElementsByTagName('html')[0].style.height='100%'
-    document.body.style.height='100%'
-    document.body.className = 'dark'
-    document.getElementById('root').style.height='100%'
-    return(
-        <div className='FullPicture'>
-            <img
-                style={{margin:'auto'}}
-                src={`${props.location.pathname}`}
-                alt='There is a beautiful car'
-            />
-        </div>
-    )
+  document.getElementsByTagName('html')[0].style.height='100%'
+  document.body.style.height='100%'
+  document.body.className = 'dark'
+  document.getElementById('root').style.height='100%'
+  return(
+    <div className='FullPicture'>
+      <img
+        style={{margin:'auto'}}
+        src={`${props.location.pathname}`}
+        alt='There is a beautiful car'
+      />
+    </div>
+  )
 }
 
 export default FullPicture
--- src/Components/Picture.jsx
+++ src/Components/Picture.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Picture({itemName}) {
 
-    return(
-        <div className='Flexible'>
-            <img
-                src={itemName}
-                width='600'
-                alt='There is a beautiful car'
-            />
-        </div>
-    )
+  return(
+    <div className='Flexible'>
+      <img
+        src={itemName}
+        width='600'
+        alt='There is a beautiful car'
+      />
+    </div>
+  )
 }
 
 export default Picture
