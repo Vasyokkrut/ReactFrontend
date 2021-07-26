6cb54ce552dd9ca0794f9eeee877b78e198b868a
Vasyokkrut <vasiliybytenko@gmail.com>
2021-07-26 12:02:21 +0300

    startup logic moved from maincomponent comstructor to initialState.js file

--- src/Components/mainComponent.jsx
+++ src/Components/mainComponent.jsx
@@ -1,6 +1,5 @@
-import { Component, StrictMode } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { useEffect, StrictMode } from 'react'
 
 import './navLists/styles.scss'
 import PopUp from './popUp/popUp.jsx'
@@ -8,67 +7,27 @@ import Header from './header/header.jsx'
 import ContentRouter from './contentRouter.jsx'
 import LeftList from './navLists/leftNavList.jsx'
 import RightList from './navLists/rightNavList.jsx'
-import { userLogin } from '../Store/account/actions.js'
 import BottomNavList from './navLists/bottomNavList.jsx'
-import { setMusicVolume } from '../Store/music/actions.js'
-import { changeTheme } from '../Store/appearance/actions.js'
 import NativeAudioElement from './music/nativeAudioElement.jsx'
 
-class MainComponent extends Component {
+function MainComponent({isDarkTheme}) {
+  useEffect(() => {
+    document.body.className = isDarkTheme ? 'dark-body' : 'light-body'
+  }, [isDarkTheme])
 
-  constructor(props) {
-    super(props)
-    document.body.className = props.isDarkTheme ? 'dark-body' : 'light-body'
-    if(localStorage.getItem('isDarkTheme') === 'false') props.changeTheme()
-
-    const userName = localStorage.getItem('userName')
-    if (userName) this.props.userLogin({userName})
-
-    const mobileDeviceNames = [
-      /android/i,
-      /iphone/i,
-      /ipad/i,
-      /ipod/i,
-      /windows phone/i,
-      /blackberry/i,
-      /tablet/i,
-      /touch/i
-    ]
-    const isMobileDevice = mobileDeviceNames.some(item => {
-      return navigator.userAgent.match(item)
-    })
-
-    // if user have mobile device, native audio element should use maximum volume
-    // otherwise native audio element will use value from localstorage
-    if (isMobileDevice) {
-      this.props.setMusicVolume(1)
-    } else {
-      const musicVolume = localStorage.getItem('musicVolume')
-      if (musicVolume !== null) this.props.setMusicVolume(+musicVolume)
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.isDarkTheme !== this.props.isDarkTheme) {
-      document.body.className = this.props.isDarkTheme ? 'dark-body' : 'light-body'
-    }
-  }
-
-  render(){
-    return(
-      <StrictMode>
-        <Header />
-        <NativeAudioElement />
-        <PopUp />
-        <div className='main-component'>
-          <LeftList />
-          <ContentRouter />
-          <RightList />
-        </div>
-        <BottomNavList />
-      </StrictMode>
-    )
-  }
+  return(
+    <StrictMode>
+      <Header />
+      <NativeAudioElement />
+      <PopUp />
+      <div className='main-component'>
+        <LeftList />
+        <ContentRouter />
+        <RightList />
+      </div>
+      <BottomNavList />
+    </StrictMode>
+  )
 }
 
 const mapStateToProps = store => {
@@ -77,12 +36,4 @@ const mapStateToProps = store => {
   }
 }
 
-const mapActionsToProps = dispatch => {
-  return {
-    userLogin: bindActionCreators(userLogin, dispatch),
-    changeTheme: bindActionCreators(changeTheme, dispatch),
-    setMusicVolume: bindActionCreators(setMusicVolume, dispatch)
-  }
-}
-
-export default connect(mapStateToProps, mapActionsToProps)(MainComponent)
+export default connect(mapStateToProps)(MainComponent)
---
+++ src/Store/initialState.js
@@ -0,0 +1,50 @@
+const initialState = {
+  music: {
+    currentMusicTime: 0,
+    isMusicPlaying: false,
+    currentAudioTrack: null,
+    currentMusicVolume: .1,
+    userAudioTracks: []
+  },
+  posts: {
+    userPosts: []
+  },
+  account: {
+    userName: null
+  },
+  appearance: {
+    isDarkTheme: true,
+    isPopUpHidden: true
+  }
+}
+
+if (localStorage.getItem('isDarkTheme') === 'false') {
+  initialState.appearance.isDarkTheme = false
+}
+
+const userName = localStorage.getItem('userName')
+if (userName) initialState.account.userName = userName
+
+const mobileDeviceNames = [
+  /android/i,
+  /iphone/i,
+  /ipad/i,
+  /ipod/i,
+  /windows phone/i,
+  /blackberry/i,
+  /tablet/i,
+  /touch/i
+]
+const isMobileDevice = mobileDeviceNames.some(item => {
+  return navigator.userAgent.match(item)
+})
+// if user have mobile device, native audio element should use maximum volume
+// otherwise native audio element will use value from localstorage
+if (isMobileDevice) {
+  initialState.music.currentMusicVolume = 1
+} else {
+  const musicVolume = localStorage.getItem('musicVolume')
+  if (musicVolume !== null) initialState.music.currentMusicVolume = +musicVolume
+}
+
+export default initialState
